b56602e1f43052095225e4e185c55124e02092dd
Jonaben1 <jonaben7@gmail.com>
2023-04-06 15:22:22 +0100

    initial commit

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ async def predict(pregnancies, glucose, bloodpressure, skinthickness, insulin,
        bmi, diabetespedigree, age])
     model = joblib.load('lg_model.pkl')
     prediction = model.predict(x.reshape(1, -1))
-    return 'You have diabetes' if prediction == True else 'No diabetes detected'
+    return await 'You have diabetes' if prediction == True else 'No diabetes detected'
 
 output = gr.components.Textbox()
-app = gr.Interface(fn= await predict, inputs=['number','number','number','number','number','number','number','number'],
+app = gr.Interface(fn=predict, inputs=['number','number','number','number','number','number','number','number'],
       outputs=output)
 
 asyncio.run(app)
