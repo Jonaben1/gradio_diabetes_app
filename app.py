ab76e5faea5a7b18cac36d8116c2ec9920ba8afc
Jonaben1 <jonaben7@gmail.com>
2023-04-06 15:13:36 +0100

    initial commit

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import numpy as np
 import gradio as gr
 import joblib
 
-def predict(pregnancies, glucose, bloodpressure, skinthickness, insulin,
+async def predict(pregnancies, glucose, bloodpressure, skinthickness, insulin,
        bmi, diabetespedigree, age):
     x = np.array([pregnancies, glucose, bloodpressure, skinthickness, insulin,
        bmi, diabetespedigree, age])
     model = joblib.load('lg_model.pkl')
     prediction = model.predict(x.reshape(1, -1))
-    return 'You have diabetes' if prediction == True else 'No diabetes detected'
+    await return 'You have diabetes' if prediction == True else 'No diabetes detected'
 
 output = gr.components.Textbox()
 app = gr.Interface(fn=predict, inputs=['number','number','number','number','number','number','number','number'],
